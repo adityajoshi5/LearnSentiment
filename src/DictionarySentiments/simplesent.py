f51a7939f73f0545ec6f58eb55ea660439148e60
Aditya Joshi <adityajoshi5@gmail.com>
2015-10-31 22:04:38 +0530

    Folders

---
+++ src/DictionarySentiments/simplesent.py
@@ -0,0 +1,14 @@
+import pandas
+
+
+class SimpleSentiments():
+    def __init__(self):
+        pass
+    
+    def read_dictionary(self):
+        pass
+    
+simple = SimpleSentiments()
+
+print "Hello"
+print pandas
