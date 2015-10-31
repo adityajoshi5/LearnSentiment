4be5bde35c719b965427e8341b58ec046ef4c7c8
Aditya Joshi <adityajoshi5@gmail.com>
2015-11-01 02:52:59 +0530

    statistical

--- src/LearningSentiments/3_score_words.py
+++ src/LearningSentiments/3_score_words.py
@@ -1,8 +1,19 @@
+'''
+We assume a tab separated training file with sentence in first column and sentiment label in second column. 
+This class would learn the ratio of a word to be in positive or negative sentence.
+This ratio would then be used to compute sentiment of the whole sentence.
 
+TODO: Modify it to use bigrams
+
+'''
 class ScoreLearner():
     def __init__(self):
         pass
     
+    def learn_corpus(self, filepath):
+        pass
+        
+    
     
 if __name__ == '__main__':
-    pass
+    scores = ScoreLearner()
